d4bf36f5811402f1adbf45503abfc7354766f89f
Stanisław Chmiela <sjchmiela@gmail.com>
2020-03-17 15:57:37 +0100

    [native-component-list] Add SVG test case

--- apps/native-component-list/src/screens/Image/tests/sources.ts
+++ apps/native-component-list/src/screens/Image/tests/sources.ts
@@ -52,6 +52,12 @@ const imageTests: ImageTestGroup = {
         source: images.uri_ico,
       },
     },
+    {
+      name: `uri:.svg`,
+      props: {
+        source: images.uri_svg,
+      },
+    },
   ],
 };
 
