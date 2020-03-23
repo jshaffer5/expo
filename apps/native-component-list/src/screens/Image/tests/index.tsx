4b9df961a80cbdc568dd32c568fef75205500195
Hein Rutjes <hrutjes@gmail.com>
2020-03-23 10:51:36 +0100

    [expo-image] Add more shadow tests

--- apps/native-component-list/src/screens/Image/tests/index.tsx
+++ apps/native-component-list/src/screens/Image/tests/index.tsx
@@ -4,11 +4,20 @@ import AppearanceTests from './appearance';
 import BorderTests from './borders';
 import EventTests from './events';
 import IOSTests from './ios';
+import ShadowsTests from './shadows';
 import SourcesTests from './sources';
 
 const tests: ImageTestGroup = {
   name: 'Image',
-  tests: [AppearanceTests, BorderTests, SourcesTests, EventTests, IOSTests, AndroidTests],
+  tests: [
+    AppearanceTests,
+    BorderTests,
+    ShadowsTests,
+    SourcesTests,
+    EventTests,
+    IOSTests,
+    AndroidTests,
+  ],
 };
 
 export default tests;
